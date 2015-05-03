b034846f29072e0b9f239a2358688e1f86b3f2ee
Emin Tolga Akgöz <tolga.akgoz@gmail.com>
2015-05-03 18:06:28 +0200

    removed grunticon from GRuntfile

--- wp-content/themes/beijeri/assets/Gruntfile.js
+++ wp-content/themes/beijeri/assets/Gruntfile.js
@@ -89,7 +89,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-grunticon');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
 
