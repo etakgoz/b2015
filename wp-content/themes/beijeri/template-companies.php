43fee334be02945b6f1833960731cf74bf107ab1
Emin Tolga Akgöz <tolga.akgoz@gmail.com>
2015-05-03 17:37:36 +0200

    removed build artifcats from repo

--- wp-content/themes/beijeri/template-companies.php
+++ wp-content/themes/beijeri/template-companies.php
@@ -10,136 +10,46 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="section">
-				<div class="container">
-					<div class="row mb30">
-						<div class="col-sm-6">
-							<h3 class="title-medium br-bottom">Våra Bolag</h3>
-						</div>
-						<div class="col-sm-6">
-							<div class="clearfix">
-								<div class="pull-right xs-pull-left">
-									<nav class="portfolio-filter pf2">
-										<ul>
-											<li class="xs-pl0"><a href="#" data-filter="*" class="active"><i class="fa fa-bars"></i></a></li>
-											<li><a href="#" data-filter=".above_">Industri</a></li>
-											<li><span> / </span></li>
-											<li><a href="#" data-filter=".beyond_">Marknad</a></li>
-											<li><span> / </span></li>
-											<li class="pr0"><a href="#" data-filter=".nowhere_" class="pr0">Verkstad</a></li>
-										</ul>
-									</nav>
+			<div class="section-header page-header">
+				<div class="page-breadcrumbs-wrapper">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="pull-left">
+									<div class="page-breadcrumbs" style="padding-left: 0">
+										<a href="#">hem</a> <span class="separator"> / </span> <?php the_title( '<a class="active">', '</a>' ); ?>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+				</div>
+
+				<div class="container">
 					<div class="row">
 						<div class="col-sm-12">
-							<div id="portfolio-isotope">
-								<div class="row col-p5">
-									<div class="portfolio portfolio-animation-off">
-										<div class="col-sm-6 col-md-4 el above_ beyond_">
-											<div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/beijer.alma.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Beijer Alma</h3>
-				                            			<p class="text">Verkstadsindustri</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el above_ nowhere_">
-					                    	<div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/centro.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Centro</h3>
-				                            			<p class="text">Kakel &amp; Klinker</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el beyond_ nowhere_">
-					                    	<div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/fjarekupan.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Fjäråskupan</h3>
-				                            			<p class="text">Köksfläktar</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el above_">
-					                        <div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/gbp.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">GPB</h3>
-				                            			<p class="text">Golvmatrial</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el beyond_">
-					                        <div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/imero.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Imero</h3>
-				                            			<p class="text">metaller</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el nowhere_">
-					                        <div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/osby.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Osby Glas</h3>
-				                            			<p class="text">isolerglass</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el above_">
-					                        <div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/vattentornet.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Vattentornet</h3>
-				                            			<p class="text">Friskt vatten på jobbet</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el beyond_">
-					                        <div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/poolkungen.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Poolkungen</h3>
-				                            			<p class="text">Pool och spabad</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-					                    <div class="col-sm-6 col-md-4 el nowhere_">
-					                        <div class="portfolio-el view">
-				                            	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/company/svensk.dos.700x500.jpg" alt="project">
-				                            	<a href="portfolio_project_half.html" class="mask">
-				                            		<div class="portfolio-middle">
-				                            			<h3 class="project-title br-bottom">Svensk Dos</h3>
-				                            			<p class="text">Läkemedelsdistribution</p>
-				                            		</div>
-				                            	</a>
-											</div>
-					                    </div> <!-- END portfolio Item -->
-									</div> <!-- END portfolio -->
-								</div>
-							</div> <!-- END Portfolio Isotope -->
+						<?php the_title( '<h1 class="section-title  mb20">', '</h1>' ); ?>
+						</div>
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<p class="section-title-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi earum, neque debitis distinctio quasi sit quibusdam, itaque possimus fuga et dolorem soluta dolore rerum? Quo eum, voluptatem earum provident facere.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<section class="companies">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-8 col-sm-offset-2">
+							<div class="comapany-info clearfix">
+								<div class="pull-left"><img src=""></div></div>
+								<div class="pull-right"></div>
+							</div>
 						</div>
 					</div>
 				</div>
