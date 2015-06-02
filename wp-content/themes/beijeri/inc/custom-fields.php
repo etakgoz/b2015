282c96b8e766fb59c2edcd427a24fb125a495286
Emin Tolga Akgöz <tolga.akgoz@gmail.com>
2015-06-02 18:16:51 +0200

    added footer text options

--- wp-content/themes/beijeri/inc/custom-fields.php
+++ wp-content/themes/beijeri/inc/custom-fields.php
@@ -1,4 +1,46 @@
 <?php
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_footer-options',
+		'title' => 'Footer Options',
+		'fields' => array (
+			array (
+				'key' => 'field_556dbea5b97c0',
+				'label' => 'Footer Text',
+				'name' => 'footer_text',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'acf-options',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
+
+
+
 
 if(function_exists("register_field_group"))
 {
